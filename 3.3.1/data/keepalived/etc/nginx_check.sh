5c455582b250dae0ecc385c1550ece6751d8757e
xiongjungit <fenyunxx@163.com>
2019-12-20 12:26:35 +0800

    haproxy+keepalived实现负载均衡和高可用

--- 3.3.1/data/keepalived/etc/nginx_check.sh
+++ 3.3.1/data/keepalived/etc/nginx_check.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #counter=$(ps -C nginx --no-heading|wc -l)
-counter=$(ps ef|grep nginx|grep -v grep|wc -l)
+counter=$(ps -ef|grep nginx|grep -v grep|wc -l)
 if [ "${counter}" = "0" ]; then
     sleep 3
     #counter=$(ps -C nginx --no-heading|wc -l)
-	counter=$(ps ef|grep nginx|grep -v grep|wc -l)
+    counter=$(ps -ef|grep nginx|grep -v grep|wc -l)
     if [ "${counter}" = "0" ]; then
         #systemctl stop keepalived
         #pkill keepalived
-        ps -ef|grep keepalived|grep -v grep|awk '{print $2}'|xargs kill -9
+        ps -ef|grep keepalived|grep -v grep|awk '{print $1}'|xargs kill -9
     fi
 fi
